d83cc35b5456981bca265020ab0b4a0125461e65
Kawin Tiraborisute <kawin.ac125@gmail.com>
2024-08-05 18:42:28 +0800

    Added title

--- app.vue
+++ app.vue
@@ -1,5 +1,15 @@
+<script setup lang="ts">
+useHead({
+  titleTemplate: (titleChunk: string | undefined) => {
+    return titleChunk
+      ? `${titleChunk} - TAXi - Your Tax Companion`
+      : "TAXi - Your Tax Companion";
+  },
+});
+</script>
+
 <template>
-    <div>
-        <NuxtPage />
-    </div>
+  <div>
+    <NuxtPage />
+  </div>
 </template>
